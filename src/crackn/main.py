2cb8c152fe44f1a8bb18218b6e8c30def9eb16d7
cpear98 <36680750+cpear98@users.noreply.github.com>
2019-10-19 17:02:25 -0500

    Test Travis CI (#4)
    Modified travis ci instructions

--- src/crackn/main.py
+++ src/crackn/main.py
@@ -3,12 +3,23 @@ import sys
 from crackn.arg_parser import ArgParser
 
 if __name__ == '__main__':
+    print(sys.argv)
     parser = ArgParser(sys.argv[1:])
+    print('Initialized Parser.')
     parser.parse_cli()
+    print('Parsed CLI arguments.')
 
     if parser.flags['h']:
         parser.print_usage()
         exit(0)
 
     directory = parser.params['dir']
-    result = subprocess.run(['bandit', '-r', directory], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    print('Running Bandit...')
+    task_complete = False
+    while(not task_complete):
+        try:
+            result = subprocess.run(['bandit', '-r', directory], stdout=subprocess.PIPE, stderr=subprocess.PIPE, timeout=10.0)
+            task_complete = True
+        except TimeoutError:
+            pass
+    print('Bandit analysis complete.')
