b6a0eaf10d1bfcf601b8582f0efc518e43682a8d
Chase Pearson <cpear98@gmail.com>
2019-10-18 20:41:45 -0500

    Basic cli parser and calling external process

--- src/arg_parser.py
+++ src/arg_parser.py
@@ -1,21 +1,55 @@
 import sys
 
+class InvalidArgumentException(Exception):
+    pass
+
 class ArgParser():
     def __init__(self):
-        self.arg_descriptions = {'dir': 'The directory containing the Python source code you would like to analyze.'}
-        self.arg_values = {'dir': None}
+        self.param_descriptions = {'dir': 'The directory containing the Python source code you would like to analyze.'}
+        self.params = {'dir': None}
+        self.flag_descriptions = {'h': 'Display this help message'}
+        self.flags = {'h': False}
 
     def parse_cli(self):
-        for arg in sys.argv[1:]:
-            arg_id, arg_val = arg.split('=')
-            if arg_id not in self.arg_values:
-                ArgParser.print_unrecognized_arg(arg_id)
-                self.print_usage()
-                exit(1)
-            self.arg_values[arg_id] = arg_val
+        try:
+            for arg in sys.argv[1:]:
+                if arg[0] == '-':
+                    self.parse_flag(arg[1:])
+                else:
+                    self.parse_param(arg)
+        except InvalidArgumentException as e:
+            self.print_unrecognized_arg(e)
+            self.print_usage()
+            exit(1)
+        if not self.has_basic_requirements():
+            self.print_usage()
+            exit(1)
+
+    def parse_param(self, param):
+        split_param = param.split('=')
+        if not self.is_valid_param(split_param):
+            raise InvalidArgumentException(param)
+        self.params[split_param[0]] = split_param[1]
+
+    def parse_flag(self, flag):
+        if not self.is_valid_flag(flag):
+            raise InvalidArgumentException(f'-{flag}')
+        self.flags[flag] = True
+
+    def has_basic_requirements(self):
+        # TODO: stub
+        if self.params['dir'] is not None:
+            return True
+        return False
     
     def print_usage(self):
         print('Temp usage message')
+
+    def is_valid_flag(self, flag):
+        return len(flag) == 1 and flag in self.flags
+
+    def is_valid_param(self, param):
+        return len(param) == 2 and param[0] != '' and param[1] != '' and param[0] in self.params
 
     @staticmethod
     def print_unrecognized_arg(arg):
--- src/main.py
+++ src/main.py
@@ -4,6 +4,10 @@ from arg_parser import ArgParser
 if __name__ == '__main__':
     parser = ArgParser()
     parser.parse_cli()
-    directory = parser.arg_values['dir']
-    result = subprocess.run(['bandit', '-r', directory])
-    print(result)
+
+    if parser.flags['h']:
+        parser.print_usage()
+        exit(0)
+
+    directory = parser.params['dir']
+    result = subprocess.run(['bandit', '-r', directory], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
