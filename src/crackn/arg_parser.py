134d068ac7cf5961abc7682bd8597f147e9b5f7b
Chase Pearson <cpear98@gmail.com>
2019-10-19 00:41:28 -0500

    Added unit tests

--- src/crackn/arg_parser.py
+++ src/crackn/arg_parser.py
@@ -2,13 +2,22 @@ class InvalidArgumentException(Exception):
     pass
 
 class ArgParser():
-    def __init__(self, args):
+    def __init__(self, args, check_assertions=False):
         self.args = args
-        self.param_descriptions = {'dir': 'The directory containing the Python source code you would like to analyze.'}
-        self.param_values = {'dir': None} # dictionary mapping parameters to a set of values they can take. if any value is allowed, the param has a value of None
-        self.params = {'dir': None}
+        self.param_descriptions = {'dir': 'The directory containing the Python source code you would like to analyze.',
+                                   'language': 'The language the source code to be analyzed is written in.'}
+        self.param_values = {'dir': None,
+                             'language': {'Python'}} # dictionary mapping parameters to a set of values they can take. if any value is allowed, the param has a value of None
+        self.params = {'dir': None, 'language': 'Python'}
         self.flag_descriptions = {'h': 'Display this help message'}
         self.flags = {'h': False}
+        self.check_assertions = check_assertions
+        if check_assertions:
+            self.check_shape()
+
+    def check_shape(self): # checks runs assertions to ensure class invariants are held
+        assert(self.param_descriptions.keys() == self.param_values.keys() == self.params.keys())
+        assert(self.flag_descriptions.keys() == self.flags.keys())
 
     def parse_cli(self):
         try:
@@ -24,17 +33,23 @@ class ArgParser():
         if not self.has_basic_requirements():
             self.print_usage()
             exit(1)
+        if self.check_assertions:
+            self.check_shape()
 
     def parse_param(self, param):
         split_param = param.split('=')
         if not self.is_valid_param(split_param):
             raise InvalidArgumentException(param)
         self.params[split_param[0]] = split_param[1]
+        if self.check_assertions:
+            self.check_shape()
 
     def parse_flag(self, flag):
         if not self.is_valid_flag(flag):
             raise InvalidArgumentException(f'-{flag}')
         self.flags[flag] = True
+        if self.check_assertions:
+            self.check_shape()
 
     def has_basic_requirements(self):
         if self.params['dir'] is not None:
@@ -48,7 +63,30 @@ class ArgParser():
         return len(flag) == 1 and flag in self.flags
 
     def is_valid_param(self, param):
-        return len(param) == 2 and param[0] != '' and param[1] != '' and param[0] in self.params and (self.param_values[param[0]] is None or param[1] in self.param_values[0])
+        return len(param) == 2 and param[0] != '' and param[1] != '' and param[0] in self.params and (self.param_values[param[0]] is None or param[1] in self.param_values[param[0]])
+
+    def add_param(self, param, values, default=None, desc=''):
+        # TODO: add param sanitation before adding
+        if param in self.params:
+            print(f'Parameter {param} already exists.')
+            return False
+        self.params[param] = default
+        self.param_values[param] = values
+        self.param_descriptions[param] = desc
+        if self.check_assertions:
+            self.check_shape()
+        return True
+
+    def add_flag(self, flag, desc=''):
+        # TODO: more flag sanitation
+        if flag in self.flags:
+            print(f'Flag {flag} already exists.')
+            return False
+        self.flags[flag] = False
+        self.flag_descriptions[flag] = desc
+        if self.check_assertions:
+            self.check_shape()
+        return True
 
     @staticmethod
     def print_unrecognized_arg(arg):
--- src/test/test_arg_parser.py
+++ src/test/test_arg_parser.py
@@ -1,80 +1,123 @@
 import unittest
-from crackn.arg_parser import ArgParser, InvalidArgumentException
+from crackn.arg_parser import ArgParser
 
 class TestArgParser(unittest.TestCase):
+    # Test ArgParser.parse_cli method
     def test_parse_cli_exits_with_no_args(self):
         args = []
-        parser = ArgParser(args)
+        parser = ArgParser(args, check_assertions=True)
         with self.assertRaises(SystemExit) as sys_exit:
             parser.parse_cli()
         self.assertEqual(sys_exit.exception.code, 1)
 
     def test_parse_cli_exits_with_bad_param(self):
         args = ['bad_param=bad_val']
-        parser = ArgParser(args)
+        parser = ArgParser(args, check_assertions=True)
         with self.assertRaises(SystemExit) as sys_exit:
             parser.parse_cli()
         self.assertEqual(sys_exit.exception.code, 1)
 
     def test_parse_cli_exits_with_bad_param_value(self):
         args = ['good_param=bad_val']
-        parser = ArgParser(args)
-        parser.param_values['good_param'] = {'good_val'} # add good_param to the parser as a valid command line flag with "good_val" as a valid value
+        parser = ArgParser(args, check_assertions=True)
+        parser.add_param('good_param', {'good_val'}) # add good_param to the parser as a valid command line flag with "good_val" as a valid value
         with self.assertRaises(SystemExit) as sys_exit:
             parser.parse_cli()
         self.assertEqual(sys_exit.exception.code, 1)
 
     def test_parse_cli_exits_with_long_flag(self):
         args = ['-long']
-        parser = ArgParser(args)
+        parser = ArgParser(args, check_assertions=True)
         with self.assertRaises(SystemExit) as sys_exit:
             parser.parse_cli()
         self.assertEqual(sys_exit.exception.code, 1)
 
     def test_parse_cli_exits_with_non_existant_flag(self):
         args = ['-w']
-        parser = ArgParser(args)
+        parser = ArgParser(args, check_assertions=True)
         with self.assertRaises(SystemExit) as sys_exit:
             parser.parse_cli()
         self.assertEqual(sys_exit.exception.code, 1)
 
     def test_parse_cli_exits_with_random_args(self):
         args = ['random1', 'random2', 'random3']
-        parser = ArgParser(args)
+        parser = ArgParser(args, check_assertions=True)
         with self.assertRaises(SystemExit) as sys_exit:
             parser.parse_cli()
         self.assertEqual(sys_exit.exception.code, 1)
 
     def test_parse_cli_exits_with_good_param_bad_flag(self):
         args = ['good_param=good_value', '-w']
-        parser = ArgParser(args)
-        parser.param_values['good_param'] = {'good_val'} # add good_param to the parser as a valid command line flag with "good_val" as a valid value
+        parser = ArgParser(args, check_assertions=True)
+        parser.add_param('good_param', {'good_val'}) # add good_param to the parser as a valid command line flag with "good_val" as a valid value
         with self.assertRaises(SystemExit) as sys_exit:
             parser.parse_cli()
         self.assertEqual(sys_exit.exception.code, 1)
 
     def test_parse_cli_exits_with_good_flag_bad_param(self):
         args = ['-g', 'bad_param=bad_val']
-        parser = ArgParser(args)
-        parser.flags['g'] = False # add g to the parser as a valid command line flag
+        parser = ArgParser(args, check_assertions=True)
+        parser.add_flag('g') # add g to the parser as a valid command line flag
         with self.assertRaises(SystemExit) as sys_exit:
             parser.parse_cli()
         self.assertEqual(sys_exit.exception.code, 1)
 
+    def test_parse_cli_good_params(self):
+        args = ['dir=test', 'good_param=good_val2']
+        parser = ArgParser(args, check_assertions=True)
+        parser.add_param('good_param', {'good_val1', 'good_val2'})
+        parser.parse_cli()
+        oracle = {'dir': 'test', 'language': 'Python', 'good_param': 'good_val2'}
+        self.assertDictEqual(parser.params, oracle)
+
+    def test_parse_cli_one_good_flag(self):
+        args = ['dir=test', '-g']
+        parser = ArgParser(args, check_assertions=True)
+        parser.add_flag('g')
+        parser.parse_cli()
+        param_oracle = {'dir': 'test', 'language': 'Python'}
+        flag_oracle = {'h': False, 'g': True}
+        self.assertDictEqual(parser.params, param_oracle)
+        self.assertDictEqual(parser.flags, flag_oracle)
+
+    def test_parse_cli_multiple_good_params(self):
+        args = ['dir=test', 'good_param1=val1', 'good_param2=val3']
+        parser = ArgParser(args, check_assertions=True)
+        parser.add_param('good_param1', {'val1', 'val2'})
+        parser.add_param('good_param2', {'val3', 'val4'})
+        parser.parse_cli()
+        param_oracle = {'dir': 'test', 'language': 'Python', 'good_param1': 'val1', 'good_param2': 'val3'}
+        self.assertDictEqual(parser.params, param_oracle)
+
+    def test_parse_cli_multiple_good_flags(self):
+        args = ['dir=test', '-g', '-t', '-v'] # currently -g, -t, -v have no real functionality. if these flags are added as actual functional flags, this test should be updated
+        parser = ArgParser(args, check_assertions=True)
+        parser.add_flag('g')
+        parser.add_flag('t')
+        parser.add_flag('v')
+        parser.parse_cli()
+        flag_oracle = {'g': True, 't': True, 'v': True, 'h': False}
+        self.assertDictEqual(flag_oracle, parser.flags)
+
+    # Test ArgParser.parse_param method
     def test_parse_param(self):
         pass
 
+    # Test ArgParser.parse_flag method
     def test_parse_flag(self):
         pass
 
+    # Test ArgParser.has_basic_requirements method
     def test_has_basic_requirements(self):
         pass
 
+    # Test ArgParser.is_valid_flag method
     def test_is_valid_flag(self):
         pass
 
+    # Test ArgParser.is_valid_param method
     def test_is_valid_param(self):
         pass
 
 if __name__ == '__main__':
-    unittest.main()
+    unittest.main(buffer=True)
