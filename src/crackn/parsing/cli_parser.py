35d8191e21e22b619084c94932f29c471b444c2d
Chase Pearson <cpear98@gmail.com>
2019-10-31 10:44:45 -0500

    Added ugly help and usage messages

--- src/crackn/parsing/cli_parser.py
+++ src/crackn/parsing/cli_parser.py
@@ -1,11 +1,37 @@
 from enum import Enum
 
+# Helper method for getting an enum value from a string
+def enum_from_string(enum_dict, string):
+    string = string.lower()
+    if string in enum_dict:
+        return enum_dict[string]
+    return None
+
 # The languages supported by CRACKn
-class Languages(Enum):
+class Language(Enum):
     PYTHON = 0
 
-class Frameworks(Enum):
+    @classmethod
+    def from_string(self, string):
+        return enum_from_string({'python': self.PYTHON}, string)
+
+    @classmethod
+    def get_string(self, string):
+        language_dict = {self.PYTHON: 'Python'}
+        return language_dict[string]
+
+# The frameworks supported by CRACKn
+class Framework(Enum):
     UNITTEST = 0
+
+    @classmethod
+    def from_string(self, string):
+        return enum_from_string({'unittest': self.UNITTEST}, string)
+
+    @classmethod
+    def get_string(self, string):
+        framework_dict = {self.UNITTEST: 'unittest'}
+        return framework_dict[string]
 
 class InvalidArgumentException(Exception):
     pass
@@ -14,6 +40,9 @@ class NonExistantParameterException(Exception):
     pass
 
 class NonExistantFlagException(Exception):
+    pass
+
+class MissingRequiredParameterException(Exception):
     pass
 
 class Parameter():
@@ -28,7 +57,10 @@ class Parameter():
             self.check_shape()
 
     def is_valid_value(self, value):
-        return self.available_values is None or value in self.available_values
+        return self.available_values is None \
+               or value in self.available_values \
+               or Language.from_string(value) in self.available_values \
+               or Framework.from_string(value) in self.available_values
 
     def set_value(self, value):
         if self.is_valid_value(value):
@@ -78,8 +110,8 @@ class Flag():
 # instantiate parameters as objects
 param_source_dir = Parameter('sdir', 'The directory containing the Python source code you would like to analyze.', None, None, required=True)
 param_test_dir = Parameter('tdir', 'The directory containing the unit tests for the source code in src-dir', None, None, required=True)
-param_language = Parameter('lang', 'The language the source code to be analyzed is written in.', {Languages.PYTHON}, Languages.PYTHON)
-param_test_framework = Parameter('framework', 'The test framework the supplied unit tests are written with.', {Frameworks.UNITTEST}, Frameworks.UNITTEST)
+param_language = Parameter('lang', 'The language the source code to be analyzed is written in.', {Language.PYTHON}, Language.PYTHON)
+param_test_framework = Parameter('framework', 'The test framework the supplied unit tests are written with.', {Framework.UNITTEST}, Framework.UNITTEST)
 parameters = {param_source_dir, param_test_dir, param_language, param_test_framework}
 
 # instantiate flags as objects
@@ -93,18 +125,18 @@ class CLIParser():
         self.args = args
 
         # map parameter names to their data structure for constant time access
-        self.parameters = dict()
+        self._parameters = dict()
         for parameter in parameters:
-            self.parameters[parameter.name] = parameter
+            self._parameters[parameter.name] = parameter
 
         # map flag names to their data structure for constant time access
-        self.flags = dict()
+        self._flags = dict()
         for flag in flags:
             # check if short and long ids exist and if they do map both to their data structure so it can be access via either name
             if flag.short is not None:
-                self.flags[flag.short] = flag
+                self._flags[flag.short] = flag
             if flag.long is not None:
-                self.flags[flag.long] = flag
+                self._flags[flag.long] = flag
 
         # check_assertions should be false in a production environment
         self.check_assertions = check_assertions
@@ -138,7 +170,10 @@ class CLIParser():
             exit(1)
 
         # after parsing all arguments, check to see if the required parameters are set
-        if not self.has_basic_requirements():
+        try:
+            self.has_basic_requirements()
+        except MissingRequiredParameterException as e:
+            print(f'[ERROR]: Missing required parameter: {e}.')
             self.print_usage()
             exit(1)
 
@@ -166,60 +201,111 @@ class CLIParser():
             self.check_shape()
 
     def has_basic_requirements(self):
-        for parameter in self.parameters:
-            param_obj = self.parameters[parameter]
-            if param_obj.required and param_obj.get_value() is None:
-                return False
-        return True
+        if self.get_flag('-h').get_value():
+            return
+        for parameter in self.get_parameters():
+            if parameter.required and parameter.get_value() is None:
+                raise MissingRequiredParameterException(parameter.name)
     
     def print_usage(self):
-        print('Usage: crackn src=')
+        print('Usage: crackn sdir=<source directory> tdir=<test directory> [parameters] [flags]')
 
     def print_help(self):
         self.print_usage()
+        print('\nPARAMETERS\n')
+        print('Parameters have the form param=value where "param" is the parameter name and "value" is the value to assign to it.\n')
+
+        # print parameters
+        for parameter in self.get_parameters():
+            entry = f'{parameter.name}   {parameter.description}\nPossible values: '
+            values = ''
+            if parameter.available_values is None:
+                values = 'Any value\n'
+            else:
+                values = '['
+                for value in parameter.available_values:
+                    if type(value) == Language:
+                        values += Language.get_string(value)
+                    elif type(value) == Framework:
+                        values += Framework.get_string(value)
+                    else:
+                        values += value
+                    values += ', '
+                # remove the ', ' from the end and close the brackets
+                values = values[:-2] + ']'
+            entry += values
+            print(entry)
+
+        print('\nFLAGS\n')
+        print('Flags have the form -f or --flag.\n')
+
+        # print flags
+        for flag in self.get_flags():
+            entry = ''
+            if flag.short is not None:
+                entry += flag.short
+            if flag.long is not None:
+                if len(entry) > 0:
+                    entry += f' ({flag.long})'
+                else:
+                    entry += flag.long
+            entry += f'   {flag.description}\n'
+            print(entry)
 
     def is_valid_flag(self, flag):
-        return flag in self.flags
+        return flag in self._flags
 
     def is_valid_param(self, param):
         # param is a list with form [name, val] where the original input was name=val
-        return len(param) == 2 and param[0] != '' and param[1] != '' and param[0] in self.parameters and self.parameters[param[0]].is_valid_value(param[1])
+        return len(param) == 2 and param[0] != '' and param[1] != '' and param[0] in self._parameters and self.get_parameter(param[0]).is_valid_value(param[1])
 
     def add_param(self, param, values, default=None, desc=''):
         # TODO: add param sanitation before adding
-        if param in self.parameters:
+        if param in self._parameters:
             print(f'Parameter {param} already exists.')
             return False
-        self.parameters[param] = Parameter(param, desc, values, default)
+        self._parameters[param] = Parameter(param, desc, values, default)
         if self.check_assertions:
             self.check_shape()
         return True
 
     def add_flag(self, short_id, long_id, desc=''):
         # TODO: more flag sanitation
-        if short_id in self.flags or long_id in self.flags:
+        if short_id in self._flags or long_id in self._flags:
             print(f'Flag ({short_id}, {long_id}) already exists.')
             return False
         new_flag = Flag(short_id, long_id, desc)
         if new_flag.short is not None:
-            self.flags[short_id] = new_flag
+            self._flags[short_id] = new_flag
         if new_flag.long is not None:
-            self.flags[long_id] = new_flag
+            self._flags[long_id] = new_flag
         if self.check_assertions:
             self.check_shape()
         return True
 
     def get_parameter(self, param):
         assert(type(param) == str)
-        if param in self.parameters:
-            return self.parameters[param]
+        if param in self._parameters:
+            return self._parameters[param]
         raise NonExistantParameterException(param)
+
+    def get_parameters(self):
+        return [self._parameters[key] for key in self._parameters.keys()]
 
     def get_flag(self, flag):
         assert(type(flag) == str)
-        if flag in self.flags:
-            return self.flags[flag]
+        if flag in self._flags:
+            return self._flags[flag]
         raise NonExistantFlagException(flag)
+
+    def get_flags(self):
+        seen = set()
+        for key in self._flags:
+            flag = self.get_flag(key)
+            if flag in seen:
+                pass
+            seen.add(flag)
+        return list(seen)
 
     @staticmethod
     def print_unrecognized_arg(arg):
